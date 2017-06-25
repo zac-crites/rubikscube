59222310b4bc1e8566df894d2923f9f4c98700f7
Unknown <zcrites@umich.edu>
2017-06-25 11:08:56 -0400

    Update button layout

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-window.onload = function()
-{
+window.onload = function () {
     var renderer = new CubeRenderer2d();
     var cube = new Cube();
 
@@ -7,24 +6,37 @@ window.onload = function()
 
     var buttons = document.getElementById("buttons");
 
-    function addButton ( text, cubecommand )
-    {
+    function addButton(text, cubecommand) {
         var button = document.createElement("button");
         button.innerHTML = text;
-        button.onclick = function()
-        {
+        button.onclick = function () {
             cubecommand();
             renderer.UpdateCubelets();
         }
-        buttons.appendChild( button );
+        buttons.appendChild(button);
     }
 
-    addButton("X", () => cube.X() );
-    addButton("X'", () => cube.Xi() );
-    addButton("U", () => cube.U() );
-    addButton("U'", () => cube.Ui() ); 
-    addButton("Z", () => cube.Z() ); 
-    addButton("Z'", () => cube.Zi() ); 
-    addButton("F", () => cube.F() ); 
-    addButton("F'", () => cube.Fi() ); 
+    function addHeader( text ) {
+        var e = document.createElement("div");
+        e.style.cssFloat = "clear";
+        var header = document.createElement("h3");
+        header.innerHTML = text;
+        buttons.appendChild(e);
+        buttons.appendChild( header );
+        buttons.appendChild(e);
+    }
+
+    addHeader("Single Edge Rotations");
+    addButton("U", () => cube.U());
+    addButton("U'", () => cube.Ui());
+    addButton("F", () => cube.F());
+    addButton("F'", () => cube.Fi());
+
+    addHeader("Full Cube Rotations");
+    addButton("X", () => cube.X());
+    addButton("X'", () => cube.Xi());
+    addButton("Y", () => cube.Y());
+    addButton("Y'", () => cube.Yi());
+    addButton("Z", () => cube.Z());
+    addButton("Z'", () => cube.Zi());
 };
