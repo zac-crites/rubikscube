5ea52df4a929e0560f59a39da56db2119cf1ccf1
Unknown <zcrites@umich.edu>
2017-06-29 22:15:02 -0400

    Move timing stuff into stopwatch

--- Stopwatch.js
+++ Stopwatch.js
@@ -1,21 +1,60 @@
-function Stopwatch() {
-    var _startTime;
-    var _stopTime;
+function Stopwatch(_display) {
+
+    var _countdownDuration = 15000;
+    var _countdownStartTime;
+    var _solveStartTime;
+    var _solveFinishTime;
     var _ticking = false;
+    var _solving = false;
+
+    var UpdateDisplay = () => {
+        if (!_ticking)
+            return;
+        requestAnimationFrame(UpdateDisplay);
+
+        if (!_solving) {
+            var elapsed = new Date().getTime() - _countdownStartTime;
+            if (elapsed < _countdownDuration) {
+                _display.innerHTML = "-" + (_countdownDuration/1000 - Math.floor(elapsed / 1000));
+            }
+            else {
+                this.SolveStart(_countdownStartTime + _countdownDuration);
+            }
+        }
+        else {
+            _display.innerHTML = (Math.floor(new Date().getTime() - _solveStartTime ) / 1000).toFixed(2);
+        }
+    }
 
     this.Start = () => {
-        _startTime = new Date().getTime();
+        _countdownStartTime = new Date().getTime();
         _ticking = true;
+        _solving = false;
+        requestAnimationFrame(UpdateDisplay);
+    }
+
+    this.SolveStart = (solveStart) => {
+        _ticking || this.Start();
+        _solving = true;
+        _solveStartTime = solveStart !== undefined ? solveStart : new Date().getTime();
     }
 
     this.Stop = () => {
-        _stopTime = new Date().getTime();
+        _solveFinishTime = new Date().getTime();
+        
+        if( _solving )
+            _display.innerHTML = (Math.floor(_solveFinishTime - _solveStartTime ) / 1000).toFixed(2);
+
         _ticking = false;
+        _solving = false;
     }
 
-    this.GetElapsed = () => {
-        if( _startTime === undefined )
-            return -1;
-        return _ticking ? new Date().getTime() - _startTime : _stopTime - _startTime; 
-    }  
+    this.Reset = () => {
+        _display.innerHTML = "&nbsp;"
+        _ticking = false;
+        _solving = false;
+    }
+
+    this.IsTicking = () => _ticking;
+    this.IsSolving = () => _solving;
 }
--- index.js
+++ index.js
@@ -2,24 +2,20 @@ window.onload = function () {
     var cube = new Cube();
     var renderer3d = new CubeRenderer(cube);
     var buttons = new KeyboardControls();
-
-    var timer = document.getElementById("timerDisplay");
-    var stopwatch = new Stopwatch();
+    var stopwatch = new Stopwatch(document.getElementById("timerDisplay"));
     var scrambling = false;
-    var solving = false;
-    var timerUpdating = false;
 
-    buttons.AddButton("Z'", () => CubeTurn(() => renderer3d.Zi()), 81, "Q");
+    buttons.AddButton("Z'", () => scrambling || renderer3d.Zi(), 81, "Q");
     buttons.AddButton("B", () => FaceTurn(() => renderer3d.B()), 87, "W");
     buttons.AddButton("L'", () => FaceTurn(() => renderer3d.Li()), 69, "E");
     buttons.AddButton("I'", () => FaceTurn(() => renderer3d.Ii()), 82, "R");
     buttons.AddButton("", () => { }, 84, "T");
-    buttons.AddButton("X", () => CubeTurn(() => renderer3d.X()), 89, "Y");
+    buttons.AddButton("X", () => scrambling || renderer3d.X(), 89, "Y");
     buttons.AddButton("r", () => FaceTurn(() => renderer3d.r()), 85, "U");
     buttons.AddButton("R", () => FaceTurn(() => renderer3d.R()), 73, "I");
     buttons.AddButton("B'", () => FaceTurn(() => renderer3d.Bi()), 79, "O");
-    buttons.AddButton("Z", () => CubeTurn(() => renderer3d.Z()), 80, "P");
-    buttons.AddButton("Y'", () => CubeTurn(() => renderer3d.Yi()), 65, "A");
+    buttons.AddButton("Z", () => scrambling || renderer3d.Z(), 80, "P");
+    buttons.AddButton("Y'", () => scrambling || renderer3d.Yi(), 65, "A");
     buttons.AddButton("D", () => FaceTurn(() => renderer3d.D()), 83, "S");
     buttons.AddButton("L", () => FaceTurn(() => renderer3d.L()), 68, "D");
     buttons.AddButton("U'", () => FaceTurn(() => renderer3d.Ui()), 70, "F");
@@ -28,13 +24,13 @@ window.onload = function () {
     buttons.AddButton("U", () => FaceTurn(() => renderer3d.U()), 74, "J");
     buttons.AddButton("R'", () => FaceTurn(() => renderer3d.Ri()), 75, "K");
     buttons.AddButton("D'", () => FaceTurn(() => renderer3d.Di()), 76, "L");
-    buttons.AddButton("Y", () => CubeTurn(() => renderer3d.Y()), 186, ";");
+    buttons.AddButton("Y", () => scrambling || renderer3d.Y(), 186, ";");
     buttons.AddButton("📹", () => renderer3d.ResetCamera(), 90, "Z", "Reset camera");
     buttons.AddButton("", () => { }, -1, "X");
     buttons.AddButton("", () => { }, -1, "C");
     buttons.AddButton("I", () => FaceTurn(() => renderer3d.I()), 86, "V");
     buttons.AddButton("", () => { }, -1, "B");
-    buttons.AddButton("X'", () => CubeTurn(() => renderer3d.Xi()), 78, "N");
+    buttons.AddButton("X'", () => scrambling || renderer3d.Xi(), 78, "N");
     buttons.AddButton("r'", () => FaceTurn(() => renderer3d.ri()), 77, "M");
     buttons.AddButton("", () => { }, -1, ",");
     buttons.AddButton("", () => { }, 190, ".");
@@ -43,54 +39,19 @@ window.onload = function () {
     renderer3d.AddAnimationCompletedListener(() => {
         if (scrambling && !renderer3d.IsAnimating()) {
             scrambling = false;
-            StartTimer();
+            stopwatch.Start();
         }
         if (cube.IsSolved()) {
-            timerUpdating = false;
+            stopwatch.Stop();
         }
     });
-
-    function StartTimer() {
-        timerUpdating = true;
-        stopwatch.Start();
-        requestAnimationFrame(UpdateTimer);
-    }
-
-    function UpdateTimer() {
-        if (!timerUpdating)
-            return;
-
-        var elapsed = stopwatch.GetElapsed();
-        if (!solving) {
-            if (elapsed < 15000) {
-                timer.innerHTML = "-" + (15 - Math.floor(elapsed / 1000));
-            }
-            else {
-                solving = true;
-                stopwatch.Start();
-            }
-        }
-        else {
-            timer.innerHTML = (Math.floor(elapsed / 10) / 100).toFixed(2);
-        }
-
-        requestAnimationFrame(UpdateTimer);
-    }
-
-    function CubeTurn(move) {
-        if (scrambling)
-            return;
-
-        move();
-    }
 
     function FaceTurn(move) {
         if (scrambling)
             return;
 
-        if (!solving) {
-            stopwatch.Start();
-            solving = true;
+        if ( stopwatch.IsTicking() && !stopwatch.IsSolving()) {
+            stopwatch.SolveStart();
         }
         move();
     }
@@ -99,9 +60,7 @@ window.onload = function () {
         if (scrambling)
             return;
 
-        timer.innerHTML = "&nbsp;";
-        timerUpdating = false;
-        solving = false;
+        stopwatch.Reset();
         scrambling = true;
 
         var moves = []
